167728aa433c06f21ebf179e7fd9a779631f0ef2
David Riley <Dril112191@gmail.com>
2023-01-16 16:58:33 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['uuid', 'lodash']
+      external: ['uuid', 'lodash', 'react-confetti']
     }
   }
 })

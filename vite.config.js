d37dacc17cb92abca56d626b6f5094df1cefbc63
David Riley <Dril112191@gmail.com>
2023-01-16 17:24:36 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['uuid', 'lodash', 'react-confetti', 'html-react-parser']
-    }
-  }
 })
+
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       external: ['uuid', 'lodash', 'react-confetti', 'html-react-parser']
+//     }
+//   }
+// })

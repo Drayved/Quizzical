7a2b1253b71c18d11aed6e9a5a434cdbcbecb487
David Riley <Dril112191@gmail.com>
2023-01-16 19:27:07 -0500

    .

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['uuid', 'lodash', 'react-confetti', 'html-react-parser']
+      external: ['./uuid', './lodash', './react-confetti', './html-react-parser']
     }
   }
 })
